2e5aa8e528746480800ee2ea6ba2a97e27fc1673
Danny <thenumberonesonsband@gmail.com>
2025-07-11 10:52:55 -0600

    Refactor variable names in variableRefactor.js

---
+++ variableRefactor.js
@@ -0,0 +1,9 @@
+let customerName = "Alice";
+let numberOfItems = 5;
+let itemPrice = 4; // $4 per item
+let totalPrice = numberOfItems * itemPrice;
+let storeName = "SuperMart";
+
+let purchaseMessage = customerName + " bought " + numberOfItems + " items for $" + totalPrice + " at " + storeName + ".";
+
+console.log(purchaseMessage);
